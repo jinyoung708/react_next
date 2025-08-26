52ecd9929dc7dd01e45f6736c396a8f813833b35
jinyoung <89575067+jinyoung708@users.noreply.github.com>
2025-08-26 16:29:49 +0900

    Updates

--- _next/static/chunks/08babcd94a799f65.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/bc9566bee3d4266d.js",
-  "static/chunks/turbopack-b331d33828cd4be1.js"
-])
---
+++ _next/static/chunks/app/layout-6df6ea56a3a9f2dd.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{3744:()=>{},6345:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3744,23))}},e=>{e.O(0,[321,441,255,358],()=>e(e.s=6345)),_N_E=e.O()}]);
