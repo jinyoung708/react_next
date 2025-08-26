62dd4aea6c603570a44a201a06d6ca7513888e3c
jinyoung <89575067+jinyoung708@users.noreply.github.com>
2025-08-26 16:04:16 +0900

    Updates

---
+++ _next/static/chunks/89beec02f648c900.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/bc9566bee3d4266d.js",
+  "static/chunks/turbopack-f9e53ecefa0fddaa.js"
+])
---
+++ _next/static/h31JR80WjIwLSExwdB_Nh/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/89beec02f648c900.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/react_next//_next/:path+",
+        "destination": "/react_next/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
